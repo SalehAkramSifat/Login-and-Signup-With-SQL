8c4e3787900f128da4ec5bd34f2d18ce57f85337
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-10-24 22:01:30 +0600

    Inital Commit

--- app/src/main/java/com/sifat/loginandsignupsql/SignupMainActivity.kt
+++ app/src/main/java/com/sifat/loginandsignupsql/SignupMainActivity.kt
@@ -3,10 +3,7 @@ package com.sifat.loginandsignupsql
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.sifat.loginandsignupsql.databinding.ActivitySignupMainBinding
 
 class SignupMainActivity : AppCompatActivity() {
